2f322ffc130ebf96978f978563d170a944eb24d1
Ross Gardler <ross@gardler.org>
2026-01-13 19:50:52 -0800

    ci: add Playwright junit + upload artifacts for PR CI (ge-hch.3.6)

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,4 +26,9 @@ export default defineConfig({
       },
     },
   ],
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report' }],
+    ['junit', { outputFile: 'junit-report.xml' }]
+  ]
 });
